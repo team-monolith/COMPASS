2a4dcba40d234a244c843f6a393a7e15710b3a2a
COMPASS <team.monolith.2020@gmail.com>
2020-11-18 14:00:39 +0900

    UCHI

--- app/src/main/java/com/monolith/compass/MainActivity.kt
+++ app/src/main/java/com/monolith/compass/MainActivity.kt
@@ -19,8 +19,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
 
-        //上部アクションバーの削除処理
-        supportActionBar!!.hide()
-
     }
 }
