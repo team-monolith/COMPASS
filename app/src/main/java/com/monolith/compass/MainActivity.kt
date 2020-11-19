d2ec9515c68ff86f73e14eb77d37a0e2f4b441e7
COMPASS <team.monolith.2020@gmail.com>
2020-11-19 14:51:16 +0900

    UCHI インテント削除関連の処理が未記述

--- app/src/main/java/com/monolith/compass/MainActivity.kt
+++ app/src/main/java/com/monolith/compass/MainActivity.kt
@@ -1,50 +1,65 @@
 package com.monolith.compass
 
 import android.Manifest
+import android.app.Application
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.monolith.compass.ui.setting.SettingFragment
 
 
 class MainActivity : AppCompatActivity() {
+
+    val GLOBAL=MyApp.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       RequestGPSPermission()
+
+        if (RequestGPSPermission()) {
+            startLocationService()
+        }
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         //ナビゲーションバーのコントローラー設定
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
+
     }
 
     //GPSパーミッションを取得、trueが返されれば実行OK
-    private fun RequestGPSPermission():Boolean{
+    private fun RequestGPSPermission(): Boolean {
 
         // Android 6, API 23以上でパーミッションの確認
-        if(Build.VERSION.SDK_INT >= 23){
+        if (Build.VERSION.SDK_INT >= 23) {
             // 既に許可している
             if (ContextCompat.checkSelfPermission(this,
                             Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED){
+                    == PackageManager.PERMISSION_GRANTED) {
                 return true
             }
             //許可していない場合
-            else{
-                ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),1000)
+            else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
             }
-        }
-        else{
+        } else {
             return true
         }
         return false
@@ -57,7 +72,7 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 1000) {
             // 使用が許可された
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
+                startLocationService()
             } else {
                 // それでも拒否された時の対応
                 toastMake("位置情報が取得できないため終了します")
@@ -69,6 +84,19 @@ class MainActivity : AppCompatActivity() {
     fun toastMake(message: String) {
         val toast = Toast.makeText(this, message, Toast.LENGTH_LONG)
         toast.show()
+    }
+
+    //位置情報取得を開始
+    fun startLocationService() {
+        val intent = Intent(application, LocationService::class.java)
+        val test: Application=application
+        startForegroundService(intent)
+    }
+
+    //位置情報取得を終了
+    fun stopLocationService(app:Application){
+        val intent = Intent(application, LocationService::class.java)
+        stopService(intent)
     }
 
 }
--- app/src/main/java/com/monolith/compass/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/monolith/compass/ui/setting/SettingFragment.kt
@@ -1,15 +1,18 @@
 package com.monolith.compass.ui.setting
 
-import com.monolith.compass.ui.setting.SettingViewModel
-
+import android.app.Application
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.monolith.compass.LocationService
+import com.monolith.compass.MainActivity
+import com.monolith.compass.MyApp
 import com.monolith.compass.R
 
 class SettingFragment : Fragment() {
@@ -31,5 +34,15 @@ class SettingFragment : Fragment() {
         })
         return root
     }
+
+    override fun onViewCreated(view:View,savedInstanceState:Bundle?){
+        super.onViewCreated(view,savedInstanceState)
+
+        view.findViewById<Button>(R.id.btnSTOP).setOnClickListener{
+            MainActivity().stopLocationService(getContext() as Application)
+        }
+
+    }
+
 
 }
