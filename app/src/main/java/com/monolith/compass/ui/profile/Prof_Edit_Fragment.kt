fdab8334bb4fd67e0e89a9f605b7470fc1db5386
COMPASS <team.monolith.2020@gmail.com>
2021-01-18 13:40:51 +0900

    yoshida0113

--- app/src/main/java/com/monolith/compass/ui/profile/Prof_Edit_Fragment.kt
+++ app/src/main/java/com/monolith/compass/ui/profile/Prof_Edit_Fragment.kt
@@ -26,18 +26,24 @@ class Prof_Edit_Fragment : Fragment() {
         profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profile_edit, container, false)
-        val textView: TextView = root.findViewById(R.id.name_txt)
         val okbtn : Button = root.findViewById(R.id.combtn)
-        val name_text :EditText = root.findViewById(R.id.name_txtedit)
+        val name_edittext :EditText = root.findViewById(R.id.name_txtedit)
         val badge_img : ImageView = root.findViewById(R.id.badge_img)
         val frame :FrameLayout = root.findViewById(R.id.frame)
         val phrase :EditText = root.findViewById(R.id.phrase_txtedit)
+        val back_btn :Button = root.findViewById(R.id.back_btn)
+        val name_txt :TextView = root.findViewById(R.id.name_txt)
+
+
 
         val ma = activity as MainActivity?
 
-        name_text.setText(ma!!.profString[0])
+        name_edittext.setText(ma!!.profString[0])
         phrase.setText(ma!!.profString[2])
 
+        back_btn.setOnClickListener{
+            findNavController().navigate(R.id.navigation_profile)
+        }
 
 
         badge_img.setOnClickListener{
@@ -51,6 +57,15 @@ class Prof_Edit_Fragment : Fragment() {
         okbtn.setOnClickListener{
             findNavController().navigate(R.id.action_navigation_profile_edit_to_navigation_profile)
         }
+
+        val back_id =ma!!.profInt[2]/10
+        val badge_id = ma!!.profInt[2] %10
+        val background ="badge_background_$back_id"
+        val favbadge ="badge_icon_$badge_id"
+        val back = resources.getIdentifier(background,"drawable","com.monolith.compass")
+        val badge = resources.getIdentifier(favbadge,"drawable","com.monolith.compass")
+        badge_img.setBackgroundResource(back)
+        badge_img.setImageResource(badge)
         return root
     }
 }
