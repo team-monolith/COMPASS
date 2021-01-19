2d9ccbc734aa075cd4f4981b1458785e10725848
COMPASS <team.monolith.2020@gmail.com>
2021-01-19 14:56:47 +0900

    yoshida0119

--- app/src/main/java/com/monolith/compass/ui/profile/ProfCardFragment.kt
+++ app/src/main/java/com/monolith/compass/ui/profile/ProfCardFragment.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import com.monolith.compass.R
 
-class Prof_Badge_List_Fragment : Fragment() {
+
+class ProfCardFragment : Fragment() {
     private lateinit var profileViewModel: ProfileViewModel
+
 
 
     override fun onCreateView(
@@ -21,7 +22,12 @@ class Prof_Badge_List_Fragment : Fragment() {
     ): View? {
         profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_profile_badge_list, container, false)
+        val root = inflater.inflate(R.layout.fragment_profile_card, container, false)
+        val test :TextView = root.findViewById(R.id.testtxt)
+        
+
+
         return root
     }
+
 }
