d4d827cad3e9a467e40054706c8177c861e68278
team-monolith <team.monolith.2020@gmail.com>
2021-01-28 14:45:51 +0900

    かわが

--- app/src/main/java/com/monolith/compass/ui/profile/ProfCardFrameFragment.kt
+++ app/src/main/java/com/monolith/compass/ui/profile/ProfCardFrameFragment.kt
@@ -32,89 +32,108 @@ class ProfCardFrameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val ma = activity as MainActivity
 
+        chenge_color(ma.profView[1])
+
         view.findViewById<ImageView>(R.id.frame_img0).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout0).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(0)
+            ma.profView[1]= 0
             ma.profsave[1]= 0
         }
         view.findViewById<ImageView>(R.id.frame_img1).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout1).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(1)
+            ma.profView[1]= 1
             ma.profsave[1]= 1
         }
         view.findViewById<ImageView>(R.id.frame_img2).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout2).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(2)
+            ma.profView[1]= 2
             ma.profsave[1]= 2
         }
         view.findViewById<ImageView>(R.id.frame_img3).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout3).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(3)
+            ma.profView[1]= 3
             ma.profsave[1]= 3
         }
         view.findViewById<ImageView>(R.id.frame_img4).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout4).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(4)
+            ma.profView[1]= 4
             ma.profsave[1]= 4
         }
         view.findViewById<ImageView>(R.id.frame_img5).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout5).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(5)
+            ma.profView[1]= 5
             ma.profsave[1]= 5
         }
         view.findViewById<ImageView>(R.id.frame_img6).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout6).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(6)
+            ma.profView[1]= 6
             ma.profsave[1]= 6
         }
         view.findViewById<ImageView>(R.id.frame_img7).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout7).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(7)
+            ma.profView[1]= 7
             ma.profsave[1]= 7
         }
         view.findViewById<ImageView>(R.id.frame_img8).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout8).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(8)
+            ma.profView[1]= 8
             ma.profsave[1]= 8
         }
         view.findViewById<ImageView>(R.id.frame_img9).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout9).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(9)
+            ma.profView[1]= 9
             ma.profsave[1]= 9
         }
         view.findViewById<ImageView>(R.id.frame_img10).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout10).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(10)
+            ma.profView[1]= 10
             ma.profsave[1]= 10
         }
         view.findViewById<ImageView>(R.id.frame_img11).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout11).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(11)
+            ma.profView[1]= 11
             ma.profsave[1]= 11
         }
         view.findViewById<ImageView>(R.id.frame_img12).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout12).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(12)
+            ma.profView[1]= 12
             ma.profsave[1]= 12
         }
         view.findViewById<ImageView>(R.id.frame_img13).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout13).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(13)
+            ma.profView[1]= 13
             ma.profsave[1]= 13
         }
         view.findViewById<ImageView>(R.id.frame_img14).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout14).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(14)
+            ma.profView[1]= 14
             ma.profsave[1]= 14
         }
         view.findViewById<ImageView>(R.id.frame_img15).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout15).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(15)
+            ma.profView[1]= 15
             ma.profsave[1]= 15
         }
         view.findViewById<ImageView>(R.id.frame_img16).setOnClickListener{
             reset_color()
-            view.findViewById<FrameLayout>(R.id.f_layout16).setBackgroundColor(Color.rgb(255, 0, 0))
+            chenge_color(16)
+            ma.profView[1]= 16
             ma.profsave[1]= 16
         }
 
@@ -133,6 +152,28 @@ class ProfCardFrameFragment : Fragment() {
             findNavController().navigate(R.id.action_navigation_profile_card_frame_to_navigation_profile_edit)
         }
         */
+    }
+
+    fun chenge_color(ID:Int){
+        when(ID){
+            0-> view?.findViewById<FrameLayout>(R.id.f_layout0)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            1-> view?.findViewById<FrameLayout>(R.id.f_layout1)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            2-> view?.findViewById<FrameLayout>(R.id.f_layout2)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            3-> view?.findViewById<FrameLayout>(R.id.f_layout3)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            4-> view?.findViewById<FrameLayout>(R.id.f_layout4)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            5-> view?.findViewById<FrameLayout>(R.id.f_layout5)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            6-> view?.findViewById<FrameLayout>(R.id.f_layout6)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            7-> view?.findViewById<FrameLayout>(R.id.f_layout7)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            8-> view?.findViewById<FrameLayout>(R.id.f_layout8)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            9-> view?.findViewById<FrameLayout>(R.id.f_layout9)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            10-> view?.findViewById<FrameLayout>(R.id.f_layout10)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            11-> view?.findViewById<FrameLayout>(R.id.f_layout11)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            12-> view?.findViewById<FrameLayout>(R.id.f_layout12)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            13-> view?.findViewById<FrameLayout>(R.id.f_layout13)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            14-> view?.findViewById<FrameLayout>(R.id.f_layout14)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            15-> view?.findViewById<FrameLayout>(R.id.f_layout15)?.setBackgroundColor(Color.rgb(255, 0, 0))
+            16-> view?.findViewById<FrameLayout>(R.id.f_layout16)?.setBackgroundColor(Color.rgb(255, 0, 0))
+        }
     }
 
     fun reset_color(){
